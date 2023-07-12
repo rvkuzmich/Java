7cbdd8bd6a0bdb97cf652858bcb71cfdad464e05
Roman Kuzmich <rvku2mich@yandex.ru>
2023-07-12 21:39:09 +0300

    DZ4 task1 and task2 completed

---
+++ hw4/task1.java
@@ -0,0 +1,31 @@
+package hw4;
+/**
+ * Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+ */
+
+ import java.util.ArrayDeque;
+
+public class task1 {
+     public static ArrayDeque<Integer> getReverseDeque(int count) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        ArrayDeque<Integer> reverseDeque = new ArrayDeque<>();
+
+        for (int i = 0; i < count; i++) {
+            int rand = (int) (Math.random() * (100 - 1));
+            deque.addFirst(rand);
+        }
+        System.out.println(deque);
+
+        for (int i = 0; i < count; i++) {
+            reverseDeque.addFirst(deque.getFirst());
+            deque.removeFirst();
+        }
+        return reverseDeque;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getReverseDeque(10));
+
+    }
+}
