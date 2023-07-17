10d13a2cc62ec78a94361c80fc62fc734a944d4e
Roman Kuzmich <rvku2mich@yandex.ru>
2023-07-17 21:59:07 +0300

    DZ5 completed

---
+++ hw5/task.java
@@ -0,0 +1,76 @@
+package Homework.hw5;
+/*
+Написать простой класс Телефонный Справочник (с помощью HashMap), который хранит в себе список фамилий и
+телефонных номеров.
+В этот телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get() искать
+номер телефона по фамилии.
+Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+тогда при запросе такой фамилии должны выводиться все телефоны.
+
+***Желательно не добавлять лишний функционал (дополнительные поля (имя, отчество, адрес),
+взаимодействие с пользователем через консоль и т.д). Консоль использовать только для вывода результатов проверки телефонного справочника.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class task {
+    public static void main(String[] args) {
+        Map<String, ArrayList<String>> phoneBook = new HashMap<>();
+        Scanner sc = new Scanner(System.in, "cp866");
+
+        boolean book = true;
+
+        while (book) {
+            System.out.println();
+            System.out.println("Введите команду:\n1- Добавить номер в записную книгу,\n" +
+                    "2- Показать все контакты,\n" +
+                    "0- Выйти из телефонной книги: ");
+            String operation = sc.nextLine();
+            switch (operation) {
+                case "1":
+                    AddContact(phoneBook, sc);
+                    break;
+                case "2":
+                    showAllContacts(phoneBook);
+                    break;
+                case "0":
+                    book = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    // добавление контакта
+    public static void AddContact(Map<String, ArrayList<String>> db, Scanner scanner) {
+        System.out.println();
+        ArrayList<String> phone_number = new ArrayList<>();
+        System.out.println("Введите фамилию контакта: ");
+        String name = scanner.nextLine().toUpperCase();
+        System.out.println();
+        System.out.println("Введите введите номер телефона: ");
+        String number = scanner.nextLine();
+        if (!db.containsKey(name)) {
+            db.put(name, phone_number);
+        }
+        db.get(name).add(number);
+
+    }
+
+    // вывод телефонной книги
+    public static void showAllContacts(Map<String, ArrayList<String>> book) {
+        System.out.println();
+        for (String name : book.keySet()) {
+            System.out.printf("Имя: " + name + "\n");
+            ArrayList<String> phones = book.get(name);
+            for (String phone : phones) {
+                System.out.printf("Номер телефона: " + phone + "\n");
+            }
+            System.out.println();
+        }
+    }
+}
